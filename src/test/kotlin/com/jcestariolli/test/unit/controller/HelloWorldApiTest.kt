f902c17b642ee7224da6601bc02187c466d2c05f
J Cestariolli <jessica.cestariolli@gmail.com>
2024-07-28 21:32:59 -0300

    test: remove unused imports

--- src/test/kotlin/com/jcestariolli/test/unit/controller/HelloWorldApiTest.kt
+++ src/test/kotlin/com/jcestariolli/test/unit/controller/HelloWorldApiTest.kt
@@ -2,17 +2,14 @@ package com.jcestariolli.test.unit.controller
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.jcestariolli.test.controller.HelloWorldApi
 import com.jcestariolli.test.controller.dto.response.HelloWorldResponseDto
 import com.jcestariolli.test.utils.parseMvcResult
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
